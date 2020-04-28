c6290bc0997d2df0bd0782f16f9efc65c9501e2c
Binita26 <45450370+Binita26@users.noreply.github.com>
2020-04-28 09:35:30 +0530

    Add files via upload

---
+++ myFirstSpringbootApp/myFirstSpringbootApp/src/test/java/com/capgemini/myFirstSpringbootApp/MyFirstSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.myFirstSpringbootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
